c0fce64d0183b75f46aff94d335a2c945c1ea406
Adam Yosua <ay2156@ship.edu>
2015-03-28 23:33:44 -0400

    Something weird.

--- modules/networkController.lua
+++ modules/networkController.lua
@@ -4,7 +4,7 @@ local networkController = {}
 local json = require "json"
 
 local POST = "POST"
-local SERVER_URL = "http://index.html"
+local SERVER_URL = "http://127.0.0.1:8000”
 
 local function networkListener( e )
     if ( e.isError ) then
