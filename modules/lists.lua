4e758d4985c9fc8aecae54358a3096c75c7dd7d5
Ryan Yosua <rcy5017@psu.edu>
2015-03-29 04:09:53 -0400

    Lists

---
+++ modules/lists.lua
@@ -0,0 +1,19 @@
+local lists = {}
+
+local universities = {"Penn State", "Temple", "Shippensburg"}
+local skills = {"Java", "Python", "Lua", "JavaScript", "HTML", "CSS"}
+local platforms = {"Mobile", "iOS", "Android", "Web", "Windows", "Mac", "Linux"}
+
+function lists.getUniversities()
+    return universities
+end
+
+function lists.getSkills()
+    return skills
+end
+
+function lists.getPlatorms()
+    return  platforms
+end
+
+return lists
