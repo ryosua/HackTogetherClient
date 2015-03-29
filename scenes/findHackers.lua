c162e46d0bc68e413ba0d99150a67a98265433f9
Ryan Yosua <rcy5017@psu.edu>
2015-03-28 21:01:59 -0400

    Scroll view for hackers.

--- scenes/findHackers.lua
+++ scenes/findHackers.lua
@@ -3,6 +3,7 @@ local button = require "modules.button"
 local colors = require "modules.colors"
 local composer = require "composer"
 local networkController = require "modules.networkController"
+local widget = require( "widget" )
 
 local font = native.systemFont
 
@@ -21,7 +22,66 @@ function scene:create( event )
 
     local sceneTitle = display.newText("Find Hackers", W * .5, H * .2, font, TITLE_SIZE )
 
+    -- ScrollView listener
+    local function scrollListener( event )
+
+        local phase = event.phase
+        if ( phase == "began" ) then -- print( "Scroll view was touched" )
+        elseif ( phase == "moved" ) then -- print( "Scroll view was moved" )
+        elseif ( phase == "ended" ) then -- print( "Scroll view was released" )
+        end
+
+        -- In the event a scroll limit is reached...
+        if ( event.limitReached ) then
+            if ( event.direction == "up" ) then -- print( "Reached top limit" )
+            elseif ( event.direction == "down" ) then -- print( "Reached bottom limit" )
+            elseif ( event.direction == "left" ) then -- print( "Reached left limit" )
+            elseif ( event.direction == "right" ) then -- print( "Reached right limit" )
+            end
+        end
+
+        return true
+    end
+
+    local function onUserTap(e)
+        print ("User with userID: " .. e.target.id .. " tapped.")
+    end
+
+    -- Create the widget
+    local scrollView = widget.newScrollView
+    {
+        backgroundColor = { 0, 0, 0},
+        top = H * .25,
+        left = 0,
+        width = W,
+        height = 400,
+        scrollWidth = 600,
+        scrollHeight = 800,
+        listener = scrollListener
+    }
+
+    scrollView.backgroundColor = { 0, 0, 0}
+
+    scrollView.anchorX = .5
+    scrollView.anchorY = .5
+
+    -- Create a image and insert it into the scroll view
+    ---local background = display.newRect( 0, 0, 300, 400 )
+
+    local USER_NAME_SIZE = TITLE_SIZE * .8
+    local USER_TEXT_SPACING = 40
+
+    for i = 1, 20 do
+        local user = display.newText("Test User " .. i, 20, i * USER_TEXT_SPACING, font, USER_NAME_SIZE )
+        user.anchorX = 0
+        user.id = "userID" .. i
+        user:setFillColor( 1, 1, 1 )
+        user:addEventListener( "tap", onUserTap )
+        scrollView:insert( user )
+    end
+
     sceneGroup:insert( sceneTitle )
+    sceneGroup:insert( scrollView )
 end
 
 function scene:show( event )
