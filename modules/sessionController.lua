8452dc27a9cc0eadbe77897d1ebf9a81b81716a9
Ryan Yosua <rcy5017@psu.edu>
2015-03-29 03:41:55 -0400

    Fixed typo.

--- modules/sessionController.lua
+++ modules/sessionController.lua
@@ -8,7 +8,7 @@ function sessionController.createInstance()
     local uid = -1
 
     function i.getLoggedIn() return loggedIn end
-    function i.getLoggedInUser(user) return loggedInUser end
+    function i.getLoggedInUser() return loggedInUser end
     function i.getUid() return uid end
 
     function i.setLoggedIn(value) loggedIn = value end
