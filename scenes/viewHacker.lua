4e95fe470c04fbc02207fd3893a4886ebd9cc563
Ryan Yosua <rcy5017@psu.edu>
2015-03-28 23:27:31 -0400

    Display users.

--- scenes/viewHacker.lua
+++ scenes/viewHacker.lua
@@ -15,24 +15,24 @@ local scene = composer.newScene()
 function scene:create( event )
     local sceneGroup = self.view
     local params = event.params
-    local hackerName = event.params.hacker.name
+    local hacker = event.params.hacker
 
     local newtwork = networkController.createInstance()
 
     local TITLE_SIZE = 25
 
-    local sceneTitle = display.newText(hackerName, W * .5, H * .2, font, TITLE_SIZE )
+    local sceneTitle = display.newText(hacker.name, W * .5, H * .2, font, TITLE_SIZE )
 
     local TEXT_SPACING = 50
     local TEXT_X = W * .1
 
-    local emailText = display.newText("Email:", TEXT_X, sceneTitle.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
+    local emailText = display.newText("Email: " .. hacker.email, TEXT_X, sceneTitle.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
     emailText.anchorX = 0
 
-    local universityText = display.newText("University:", TEXT_X, emailText.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
+    local universityText = display.newText("University: " .. hacker.university, TEXT_X, emailText.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
     universityText.anchorX = 0
 
-    local endorsementsText = display.newText("Endorsements:", TEXT_X, universityText.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
+    local endorsementsText = display.newText("Endorsements: " .. hacker.endorsements, TEXT_X, universityText.y + TEXT_SPACING, font, TITLE_SIZE * .7 )
     endorsementsText.anchorX = 0
 
     sceneGroup:insert( sceneTitle )
